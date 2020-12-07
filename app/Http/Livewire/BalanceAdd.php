8430759db0e1e3d0039ac1c3dc37e51b84824ce1
Aiaz AA <bepctak.ru@gmail.com>
2020-12-07 21:57:33 +0600

    +

---
+++ app/Http/Livewire/BalanceAdd.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class BalanceAdd extends Component
+{
+    public function render()
+    {
+        return view('livewire.balance-add');
+    }
+}
---
+++ app/Http/Livewire/BalanceOut.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class BalanceOut extends Component
+{
+    public function render()
+    {
+        return view('livewire.balance-out');
+    }
+}
